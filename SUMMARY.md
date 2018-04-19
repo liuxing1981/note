c67141aed86579cefd05fa7b90878c047f8d65ef
liuxing <xing.liu@centling.com>
2018-04-19 14:47:38 +0800

    init

---
+++ SUMMARY.md
@@ -0,0 +1,6 @@
+# Summary
+
+* [docker](docs/docker.md)
+* [kubernetes](docs/kubernetes.md)
+* [linux shell](docs/shell.md)
+* [ssh](docs/ssh.md)
---
+++ view.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NAME=note
+docker rm -f $NAME 2>/dev/null
+docker run --name $NAME -d -p 4000:4000 \
+        -e GIT_URL=git@github.com/liuxing1981/${NAME}.git \
+        -v note:/root/project \
+	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+	liuxing1981/gitbook
+
+echo "gitbook note start at http://localhost:4000" 
