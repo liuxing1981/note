16935b19e54257c4fd2a387147ff2e42ad927411
liuxing <xing.liu@centling.com>
2018-04-19 15:50:21 +0800

    modi

--- view.sh
+++ view.sh
@@ -2,8 +2,7 @@
 NAME=note
 docker rm -fv $NAME 2>/dev/null
 docker run --name $NAME -d -p 4000:4000 \
-        -e GIT_URL=git@github.com/liuxing1981/${NAME}.git \
-        -v note:/root/project \
+        -e GIT_URL=git@github.com:liuxing1981/${NAME}.git \
 	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
 	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
 	liuxing1981/gitbook
