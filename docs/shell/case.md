78b26640b4b9eec2c23426af53fa668d0cda1c6e
liuxing <xing.liu@centling.com>
2018-04-24 17:00:54 +0800

    update .gitignore

---
+++ docs/shell/case.md
@@ -0,0 +1,15 @@
+# case
+
+```
+case var in
+    patten_1)
+    echo a
+    ;;
+    patten_2)
+    echo b
+    ;;
+    *)
+    echo c  
+    ;;
+esac
+```
--- view.sh
+++ view.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 NAME=note
 docker rm -fv $NAME 2>/dev/null
-docker run --name $NAME -d -p 4000:4000 \
+docker volume create note
+case $1 in
+    debug)
+    docker run --name $NAME -it -p 4000:4000 \
         -e GIT_URL=git@github.com:liuxing1981/${NAME}.git \
 	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
 	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+	-v note:/root/project \
+	liuxing1981/gitbook sh
+    echo "gitbook in debug mode" 
+    ;;
+    local)
+    docker run --name $NAME -d -p 4000:4000 \
+        -v `pwd`:/root/project \
 	liuxing1981/gitbook
-
-echo "gitbook note start at http://localhost:4000" 
+    echo "gitbook in local view mode"
+    ;;
+    *)
+    docker run --name $NAME -d -p 4000:4000 \
+        -e GIT_URL=git@github.com:liuxing1981/${NAME}.git \
+	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
+	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
+	-v note:/root/project \
+	liuxing1981/gitbook
+    echo "gitbook in github view mode,http://localhost:4000"
+    ;;
+esac
