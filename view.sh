56e778c8d52e01f2e0279ab8aa643ff419303798
liuxing <xing.liu@centling.com>
2018-04-24 17:19:54 +0800

    update .gitignore

--- view.sh
+++ view.sh
@@ -2,13 +2,13 @@
 NAME=note
 docker rm -fv $NAME 2>/dev/null
 docker volume create note
+#	-v note:/root/project \
 case $1 in
     debug)
     docker run --name $NAME -it -p 4000:4000 \
         -e GIT_URL=git@github.com:liuxing1981/${NAME}.git \
 	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
 	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
-	-v note:/root/project \
 	liuxing1981/gitbook sh
     echo "gitbook in debug mode" 
     ;;
@@ -23,7 +23,6 @@ case $1 in
         -e GIT_URL=git@github.com:liuxing1981/${NAME}.git \
 	-v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
 	-v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
-	-v note:/root/project \
 	liuxing1981/gitbook
     echo "gitbook in github view mode,http://localhost:4000"
     ;;
